bb61e568989068e9b89ffc748f81a4e02fad1a13
DJRHails <daniel@hails.info>
2020-03-27 14:26:54 +0000

    Add CookieConsent to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import CookieConsent from '@components/CookieConsent'
 import Container from '@components/Layout/Layout.Container'
 
 interface LayoutProps {
@@ -23,6 +24,14 @@ const Layout: React.FC<LayoutProps> = ({ children, ...rest }) => {
 
   return (
     <>
+      <CookieConsent acceptOnScroll>
+        This website uses cookies{' '}
+        <span role="img" aria-label="cookies">
+          🍪
+        </span>
+        {' '}
+        so I can enhance the user experience. Hope thats cool with you!
+      </CookieConsent>
       <Container {...rest}>{children}</Container>
     </>
   )
