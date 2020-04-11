fb5eadb8893421bd60a7e4a60628575ea99d0b52
DJRHails <daniel@hails.info>
2020-04-11 18:34:31 +0100

    Add ProjectCard with styling

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Img from 'gatsby-image'
+import _ from 'lodash'
+import { IconExternal, IconGitHub } from '@components/Icon'
+
+interface ProjectMetadata {
+  date: string
+  title: string
+  github: string
+  external: string
+  tech: string[]
+  description: string
+  company: string
+  cover: any
+}
+
+const ProjectCard: React.FC<{data: ProjectMetadata}> = ({ data }) => {
+  const { date, title, github, external, tech, description, company, cover } = data
+  console.log(data)
+  return (
+    <div id={_.kebabCase(title)} className="project-card">
+      <div className="project-card__wrapper">
+        { cover && (
+          <div className="project-card__img">
+            <a
+              href={external ? external : github ? github : '#'}
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              <Img
+                fluid={cover.childImageSharp.fluid}
+                alt={title}
+              />
+            </a>
+          </div>
+        )}
+        <div className="project-card__body">
+          <h6 className="project-card__subtitle mb-2 text-muted">
+            {date} {company && <><span>·</span> {company}</>}
+          </h6>
+          <h2 className="project-card__title">
+            {external ? (
+              <a
+                href={external}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label="External Link">
+                {title}
+              </a>
+            ) : (
+              title
+            )}
+          </h2>
+          <p className="project-card__text">{description}</p>
+          <div className="project-card__link-wrapper text-muted">
+            { github && (
+              <a
+                href={`https://github.com/${github}`}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label="GitHub Link">
+                <IconGitHub />
+              </a>
+            )}
+            { external && (
+              <a
+                href={external}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                aria-label="External Link">
+                <IconExternal />
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
