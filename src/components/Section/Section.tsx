cfae9b0e0706294fb90d4da48fc55ac118371d87
DJRHails <daniel@hails.info>
2020-04-11 18:31:17 +0100

    Build title and subtitle into the Section

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -4,14 +4,28 @@ import classNames from 'classnames'
 
 interface SectionProps {
   id: string
+  title?: string
+  subtitle?: string
   className?: string
   skew?: boolean
   arrow?: boolean
   orientation?: string[]
 }
 
+const EntryTitle: React.FC<{title: string, subtitle: string}> = ({title, subtitle}) => {
+  return ( title ?
+    <header className="entry-title">
+      <h1>{title}</h1>
+      <h3 className="subtitle">{subtitle}</h3>
+    </header>
+    : null
+  )
+}
+
 const Section: React.FC<SectionProps> = ({
   id,
+  title,
+  subtitle,
   className,
   skew,
   arrow,
@@ -26,7 +40,10 @@ const Section: React.FC<SectionProps> = ({
 
   return (
     <section id={id} className={sectionClass}>
-      {children}
+      <div className="container">
+        <EntryTitle title={title} subtitle={subtitle} />
+        {children}
+      </div>
     </section>
   )
 
