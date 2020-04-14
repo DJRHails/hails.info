f2a1c4a0854c7622e09374467a37988f73fd3271
DJRHails <daniel@hails.info>
2020-04-14 19:19:04 +0100

    Add excerpt support

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ exports.plugins = [
   {
     resolve: "gatsby-transformer-remark",
     options: {
+      "excerpt_separator": `<!-- end -->`,
       plugins: [
         "gatsby-remark-component",
         {
