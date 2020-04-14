f816557fce3098755981a885a4a8b447fdc3a495
DJRHails <daniel@hails.info>
2020-04-14 17:28:21 +0100

    Separate out ConversationInvite

---
+++ src/components/Footer/Footer.ConversationInvite.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ConversationInvite: React.FC = () => {
+  return (
+    <>
+      <p className="footer__line text-muted">WHAT NEXT?</p>
+      <h2>Let's make the conversation less one sided:</h2>
+      <h2>
+        <a href="mailto:daniel@hails.info">
+          <strong>daniel@hails.info</strong>
+        </a>
+      </h2>
+    </>
+  );
+}
+
+export default ConversationInvite;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,20 +2,7 @@ import React from "react";
 import Section from "@components/Section";
 import { IconGitHub } from "@components/Icons";
 import classNames from "classnames";
-
-const ConversationInvite: React.FC = () => {
-  return (
-    <>
-      <p className="footer__line text-muted">WHAT NEXT?</p>
-      <h2>Let's make the conversation less one sided:</h2>
-      <h2>
-        <a href="mailto:daniel@hails.info">
-          <strong>daniel@hails.info</strong>
-        </a>
-      </h2>
-    </>
-  );
-}
+import ConversationInvite from "@components/Footer/Footer.ConversationInvite";
 
 const Footer: React.FC = ({ className }) => {
   return (
