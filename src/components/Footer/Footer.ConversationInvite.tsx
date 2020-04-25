a8d7da82cb2076e9dfe92d22e9d60edcb0d66d0f
DJRHails <daniel@hails.info>
2020-04-25 22:49:44 +0100

    Make all font-sizes responsive.

--- src/components/Footer/Footer.ConversationInvite.tsx
+++ src/components/Footer/Footer.ConversationInvite.tsx
@@ -4,8 +4,9 @@ const ConversationInvite: React.FC = () => {
   return (
     <>
       <p className="footer__line text-muted">SO, WHAT NEXT?</p>
-      <h2>Let's make the conversation less one sided:</h2>
+      <h2>Let's make the conversation less one sided.</h2>
       <h2>
+        Say{" "}
         <a href="mailto:hello@hails.info">
           <strong>hello@hails.info</strong>
         </a>
